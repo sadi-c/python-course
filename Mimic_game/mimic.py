7f5c8063c22c3e37d2a02aee8e81f23a00566c61
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2021-01-24 22:47:43 -0500

    mimic

---
+++ Mimic_game/mimic.py
@@ -0,0 +1,11 @@
+msg = input("Say Something: ")
+
+
+while msg != "stop copying me":
+	print(msg)
+	msg = input()
+print("UGH!")
+
+while msg != "stop copying me":
+ 	msg = input(f"{msg}\n")
+print("UGH FINE YOU WIN!")
