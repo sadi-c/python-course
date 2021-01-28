930a656943e0357959d65fa24ebc62b1de6dc7b1
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2021-01-27 23:08:46 -0500

    website list

--- WebsiteBlocker/web.py
+++ WebsiteBlocker/web.py
@@ -3,3 +3,4 @@ from datetime import datetime as dt
 hosts_path = r"/etc/hosts"   // r is for raw string
 hosts_temp = "hosts"
 redirect = "127.0.0.1"
+web_sites_list = ["https://www.instagram.com", "instagram.com"]    
