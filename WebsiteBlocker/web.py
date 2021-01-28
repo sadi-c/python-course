e8eaf9468cec21d527c6326f3656b5719911bc81
Sadika Chowdhury <sadikachowdhury29@gmail.com>
2021-01-27 23:10:17 -0500

    condition

--- WebsiteBlocker/web.py
+++ WebsiteBlocker/web.py
@@ -4,3 +4,14 @@ hosts_path = r"/etc/hosts"   // r is for raw string
 hosts_temp = "hosts"
 redirect = "127.0.0.1"
 web_sites_list = ["https://www.instagram.com", "instagram.com"]    
+while True:
+   if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,22):
+       print("Working hours")
+       with open(hosts_path, "r+") as file:
+           content = file.read()
+           for website in web_sites_list:
+               if website in content:
+                   pass
+               else:
+                   file.write(redirect+" "+website+"\n")
+ 
